6a9abf094d7e0864474bf591f129c1d17d89446c
Yashwanth Beeravelli <yashwanthbeeravelli@gmail.com>
2015-06-22 19:07:46 -0400

    Initial commit

---
+++ app/src/main/java/d2workshop/mythirdtry/MainActivity.java
@@ -0,0 +1,56 @@
+package d2workshop.mythirdtry;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textview;
+        textview = (TextView) findViewById(R.id.textView);
+        Button okayButton= (Button) findViewById(R.id.okayButton);
+        okayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textview.setText("Some random thing.");
+            }
+        });
+
+
+        //Creating the ListView
+       View listview; //R is resources
+       ListView listView = (ListView) findViewById(R.id.listView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
